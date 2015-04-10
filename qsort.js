f4a51f129172e5f7100d02a77aca38a0ed5eb64e
Vincent <vincentschen@yahoo.com>
2015-04-10 01:03:11 -0700

    reorganized qsort file locations

--- qsort.js
+++ qsort.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var app = express();
-port = process.argv[2] || 8000;
-
-app.configure(function () {
-    app.use(
-        "/", //the URL throught which you want to access to you static content
-        express.static(qsort.html) //where your static content is located in your filesystem
-    );
-});
-app.listen(port); //the port you want to use
-console.log("Express server running");
+  $('#myCarousel').carousel({
+    interval:   4000
+  });
+  
+  var clickEvent = false;
+  $('#myCarousel').on('click', '.nav a', function() {
+      clickEvent = true;
+      $('.nav li').removeClass('active');
+      $(this).parent().addClass('active');    
+  }).on('slid.bs.carousel', function(e) {
+    if(!clickEvent) {
+      var count = $('.nav').children().length -1;
+      var current = $('.nav li.active');
+      current.removeClass('active').next().addClass('active');
+      var id = parseInt(current.data('slide-to'));
+      if(count == id) {
+        $('.nav li').first().addClass('active');  
+      }
+    }
+    clickEvent = false;
+  });
